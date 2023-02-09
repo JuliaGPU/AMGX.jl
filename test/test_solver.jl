197f7b3b7bb7b6bb45060ce27cff59d5a3158fba
Valentin Churavy <v.churavy@gmail.com>
2023-02-09 11:10:28 -0500

    WIP

--- test/test_solver.jl
+++ test/test_solver.jl
@@ -47,15 +47,22 @@ using AMGX: Config, Resources, AMGXVector, AMGXMatrix, Solver, dDDI, dFFI
         AMGX.setup!(s, M)
         AMGX.solve!(x, s, b; zero_inital_guess=true)
         @test AMGX.get_status(s) == AMGX.DIVERGED
-        @test AMGX.get_iterations_number(s) == 0
-        @test AMGX.get_iteration_residual(s) > 1.0
+
+        # Need a proper way to test for build version
+        if AMGX.build_info()[1] != "2.2.0.132-opensource"
+            @test AMGX.get_iterations_number(s) == 0
+            @test AMGX.get_iteration_residual(s) > 1.0
+        else
+            @test AMGX.get_iterations_number(s) == -1
+            @test_throws AMGX.AMGXException("Incorrect parameters for amgx call.") AMGX.get_iteration_residual(s)
+        end
 
         close(M)
         @test_throws ErrorException AMGX.solve!(x, s, b; zero_inital_guess=true)
     end
 
     @scope @testset "get_iterations_number" begin
-        c = @! Config(Dict("monitor_residual" => 1, "tolerance" => 1e-14, "store_res_history" => 1))
+        c = @! Config(Dict("monitor_residual" => 1, "tolerance" => "1e-14", "store_res_history" => 1))
         r = @! Resources(c)
         M = @! AMGXMatrix(r, mode)
         x = @! AMGXVector(r, mode)
@@ -67,8 +74,9 @@ using AMGX: Config, Resources, AMGXVector, AMGXMatrix, Solver, dDDI, dFFI
         AMGX.upload!(b, [1.0, 2.0, 4.0])
         AMGX.setup!(s, M)
         AMGX.solve!(x, s, b)
+        @show niter = AMGX.get_iterations_number(s)
         @test AMGX.get_iteration_residual(s) < 1e-14
-        niter = AMGX.get_iterations_number(s)
+        @show niter = AMGX.get_iterations_number(s)
         @test AMGX.get_iteration_residual(s, niter) == AMGX.get_iteration_residual(s)
     end
 end
